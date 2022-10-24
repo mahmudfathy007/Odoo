e9d45866fc2bb6766bf36e4da1ad0bfa1e1e0b21
Mahmoud fathy <76597828+mahmudfathy007@users.noreply.github.com>
2022-10-24 15:48:26 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,104 @@
-f = open("demofile2.txt", "a")
-f.write("1-Write what you have learned today?\n>>>I learned how to teach people using the python and the terminal\n \n2-What new skills you need?\n>>> How to use python and how to improve my iq \n\n3-What is your expectations?\n>>> by the end of the course i want to be professional in python and using it and learn the odoo\n\n4-How was the day for you?\n>>> amazing using monkey type")
-f.close()
+# # print("enter your age")
+# # age = int(input())
+# # age=age+1
+# # print("your age is" , age)
 
-#open and read the file after the appending:
-f = open("demofile2.txt", "r")
-print(f.read())
+
+
+
+
+
+# #this script is used to teach you more about data types in pyhton>>\
+# print("There are differnt basics data types in python Like")
+# print("*Integers")
+# print("*Floating")
+# print("*Strings")
+# print("*Boolean ")
+# #first type Intgers
+# print("Fist : Integers")
+# print("In Python 3, there is effectively no limit to how long an integer value can be."
+#       " Of course, it is constrained by the amount of memory "
+#       "your system has, as are all things, but beyond that an integer can be as long as you need it to be")
+# print("Like >>> print(10)10")
+# print("")
+# #Second type Floating
+# print("Second : Floating")
+# print("The float type in Python designates a floating-point number. float values are specified "
+#       "with a decimal point. Optionally, the character e or "
+#       "E followed by a positive or negative integer may be appended to specify scientific notation:")
+# print("Like >>> 1.79e308")
+# print("")
+# print("1.79e+308")
+# print("Like >>> 1.8e308")
+# print("")
+# print("inf")
+# #Third type String
+# print("Third  : Strings")
+# print("Strings are sequences of character data. The string type in Python is called str.String literals may be delimited using either single or double quotes. All the characters between the opening delimiter and matching closing delimiter are part of the string:")
+# print("also")
+# print("")
+# print("A string in Python can contain as many characters as you wish. The only limit is your machine’s memory resources. A string can also be empty:")
+# print(">>> ''")
+# print("''")
+
+
+
+
+
+#
+#
+#
+# #in this script you will learn if statment in python
+# #It used to do a line of code in condition and you use else do to it incase of failier of the first condition
+#
+#
+# #variable
+# #take the student mark
+# print("Hi please enter your mark")
+# print("we input here to take variable from user the var is his mark")
+# print("a = int(input())")
+# print("")
+# print("in this line if the mark is 0 this will display his grade as F we used == to chek if it is equal to 0")
+# print("if a ==0:")
+# print("print your grade is F")
+# print("")
+# print("in this line if the mark is 0 this will display his grade as C we used > to chek if it is bigger than 0")
+# print("elif a>0  and a<70:")
+# print("print(Your grade is C")
+# print("")
+# print("in this line if the mark is 0 this will display his grade as B")
+# print("elif a>70 and a<90:")
+# print("print(Your grade is B)")
+# print("")
+# print("in this line if the mark is no one of the previos condition this will display his grade as A")
+# print("else:")
+# print("print(Your grade is A , Congratoulition)")
+
+#
+# print("There are differnt types of errors in python")
+# print("First Error : Syntax errors")
+# print("Syntax errors are the most basic type of error. They arise when the Python parser is unable to understand a line of code. Syntax errors are almost always fatal, i.e. there is almost never a way to successfully execute a piece of code containing syntax errors. Some syntax errors can be caught and handled, like eval(""), but these are rare.In IDLE, it will highlight where the syntax error is. Most syntax errors are typos, incorrect indentation, or incorrect arguments. If you get this error, try looking at your code for any of these")
+# print()
+# print("example:")
+# print("myfunction(x, y):")
+# print("    return x + y")
+# print("else:")
+# print("    print(Hello!)")
+# print("if mark >= 50")
+# print("    print(You passed!)")
+# print("if arriving:")
+# print("    print(Hi!)")
+# print("esle:")
+# print("    print(Bye!)")
+# print("if flag:")
+# print("print(Flag is set!)")
+# print()
+# print()
+# print("Second Error : Logic  errors")
+# print("These are the most difficult type of error to find, because they will give unpredictable results and may crash your program.  A lot of different things can happen if you have a logic error. However these are very easy to fix as you can use a debugger, which will run through the program and fix any problems.A simple example of a logic error can be showcased below, the while loop will compile and run however, the loop will never finish and may crash Python:")
+# print("example:")
+# print("#Counting Sheep")
+# print("#Goal: Print number of sheep up until 101.")
+# print("sheep_count=1")
+# print("while sheep_count<100:")
+# print("    print(%i Sheep%sheep_count)")
